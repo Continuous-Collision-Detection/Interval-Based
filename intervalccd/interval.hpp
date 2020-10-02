96aa390c4dcc2aaf12a13303c6f0ef0f1e1c063e
zfergus <zfergus@nyu.edu>
2020-10-02 18:34:50 -0400

    Turned on `rounded_arith_std` for macOS
    * Removed trig rounding policy since we do not use it

--- intervalccd/interval.hpp
+++ intervalccd/interval.hpp
@@ -6,42 +6,19 @@
 #include <Eigen/Core>
 #include <boost/numeric/interval.hpp>
 
-
-
 namespace intervalccd {
 
 namespace interval_options {
     typedef boost::numeric::interval_lib::checking_base<double> CheckingPolicy;
 } // namespace interval_options
 
-#if defined(__APPLE__)
-
-// clang-format off
-#warning "Rounding modes seem to be broken with trigonometric functions on macOS, unable to compute exact interval arithmetic!"
-// clang-format on
 typedef boost::numeric::interval<
     double,
     boost::numeric::interval_lib::policies<
         boost::numeric::interval_lib::save_state<
-            boost::numeric::interval_lib::rounded_transc_exact<double>>,
+            boost::numeric::interval_lib::rounded_arith_std<double>>,
         interval_options::CheckingPolicy>>
     Interval;
-
-#else
-
-// Use proper rounding arithmetic
-typedef boost::numeric::interval<
-    double,
-    boost::numeric::interval_lib::policies<
-        boost::numeric::interval_lib::save_state<
-            boost::numeric::interval_lib::rounded_transc_std<double>>,
-        interval_options::CheckingPolicy>>
-    Interval;
-
-#endif
-
-
-
 
 } // namespace intervalccd
 
